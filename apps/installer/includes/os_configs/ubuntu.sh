b0eaa55ea599ccf4b88f8e53e16f16210c2d16b2
Francesco Borzì <borzifrancesco@gmail.com>
2020-11-24 10:56:37 +0100

    fix(Bash/CI): fix Ubuntu, Docker and macOS builds (TODO: fix Windows)

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -6,19 +6,18 @@ sudo apt-get update
 if [[ $CONTINUOUS_INTEGRATION ]]; then
   sudo apt-get -y install build-essential libtool make cmake cmake-data clang openssl libgoogle-perftools-dev \
   libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev libace-dev mysql-client \
-  libncurses5-dev libboost-system1.6*-dev libboost-program-options1.6*-dev libboost-iostreams1.6*-dev
+  libncurses5-dev libboost-all-dev
 else
   case $UBUNTU_VERSION in
     "14.04")
       sudo apt-get -y install build-essential libtool make cmake cmake-data gcc g++ clang openssl libgoogle-perftools-dev \
       libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev libace-dev mysql-server libncurses-dev \
-      curl unzip
+      curl unzip libboost-all-dev
       ;;
     *)
       sudo apt-get install -y git cmake make gcc g++ clang libmysqlclient-dev \
       libssl-dev libbz2-dev libreadline-dev libncurses-dev \
-      mysql-server libace-6.* libace-dev curl unzip \
-      libboost-system1.6*-dev libboost-program-options1.6*-dev libboost-iostreams1.6*-dev
+      mysql-server libace-6.* libace-dev curl unzip libboost-all-dev
       ;;
   esac
 fi
